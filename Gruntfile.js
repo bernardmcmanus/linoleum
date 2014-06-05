cd69949f3c9b296251ae1c1c0e32356028153db3
Bernard McManus <bmcmanus@millennialmedia.com>
2014-06-04 21:48:37 -0400

    fixing Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,45 +1,11 @@
 module.exports = function( grunt ) {
 
+
     var libs = [
         'js/linoleum.js',
         'js/tile.js'
     ];
 
-    // grunt.initConfig({
-    //
-    //         pkg: grunt.file.readJSON('package.json'),
-    //
-    //         clean : ["linoleum-*"],
-    //
-    //         uglify : {
-    //             options : {
-    //                 banner : '/*! <%= pkg.name %> - <%= pkg.version %> - <%= pkg.author %> - <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-    //             },
-    //             release : {
-    //                 files : {
-    //                     'linoleum-<%= pkg.version %>.min.js' : libs
-    //                 }
-    //             }
-    //         },
-    //
-    //         "git-describe" : {
-    //             "options" : {
-    //                 prop : "git-version"
-    //             }
-    //         },
-    //
-    //         jshint : {
-    //             all : [ 'Gruntfile.js', 'js/linoleum.js', 'js/tile.js' ]
-    //         }
-    //
-    // });
-    //
-    // grunt.loadNpmTasks('grunt-contrib-jshint');
-    // grunt.loadNpmTasks('grunt-contrib-clean');
-    // grunt.loadNpmTasks('grunt-contrib-uglify');
-    // grunt.loadNpmTasks('grunt-git-describe');
-    //
-    // grunt.registerTask('default', ['jshint','git-describe', 'clean', 'uglify']);
 
     grunt.initConfig({
 
@@ -118,6 +84,7 @@ module.exports = function( grunt ) {
     ]);
 
     grunt.registerTask( 'debug' , [
+        'dev',
         'watch'
     ]);
 
